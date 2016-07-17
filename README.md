7bb9197360aef2466bdffad49cc20c58312f5b8c
Richard Lucas <lucasweb@me.com>
2016-07-17 12:26:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+#java8-guava-collectors
+
+Java 8 collectors for the Guava collections
+
+## Setup
+
+Add the latest java8-extensions Maven dependency to your project
+
+```xml
+<dependency>
+  <groupId>uk.co.lucasweb</groupId>
+  <artifactId>java8-guava-collectors</artifactId>
+  <version>1.0</version>
+</dependency>
+```
---
+++ src/test/java/uk/co/lucasweb/stream/CollectorsTest.java
@@ -0,0 +1,54 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ *   the License. You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ *   an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *   specific language governing permissions and limitations under the License.
+ *
+ *   Copyright 2016 the original author or authors.
+ *
+ */
+
+package uk.co.lucasweb.stream;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Richard Lucas
+ */
+public class CollectorsTest {
+
+    @Test
+    public void shouldCollectObjectsIntoImmutableList() {
+        Assertions.assertThat(Stream.of("1", "2", "3")
+                                      .collect(ImmutableListCollector.toImmutableList()))
+                .isInstanceOf(ImmutableList.class)
+                .containsExactly("1", "2", "3");
+    }
+
+    @Test
+    public void shouldCollectImmutableListsIntoImmutableList() {
+        assertThat(Stream.of(ImmutableList.of("1", "2", "3"), ImmutableList.of("4", "5", "6"))
+                           .collect(ImmutableListsCollector.toImmutableList()))
+                .isInstanceOf(ImmutableList.class)
+                .containsExactly("1", "2", "3", "4", "5", "6");
+    }
+
+    @Test
+    public void shouldCollectObjectsIntoImmutableSet() {
+        assertThat(Stream.of("1", "2", "3")
+                           .collect(ImmutableSetCollector.toImmutableSet()))
+                .isInstanceOf(ImmutableSet.class)
+                .containsExactly("1", "2", "3");
+    }
+}
